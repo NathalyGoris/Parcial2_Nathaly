346eaa94b66d6fa65cdb7e55106bb648ba31fa94
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-07-10 20:11:42 -0400

    Creando el Contexto, cambiando el modelo EntradasDetalle (ya que no me di cuenta que estaba fuera del proyecto SERVER), y eliminando algunos enter de los modelos

--- Shared/EntradasDetalle.cs
+++ Shared/EntradasDetalle.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations;
 public class EntradasDetalle
 {
     [Key]
-
     public int DetalleId { get; set; }
 
     [Required (ErrorMessage = "El Id de la entrada es obligatorio")]
