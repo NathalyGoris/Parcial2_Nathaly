ef3d35fb3aed4dc4c5a9df4061f4f55f98a2c73b
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-07-10 20:33:42 -0400

    Commit pedido por el profe de lo que teniamos, yo estoy en proceso del controller de EntradasDetalle

---
+++ Server/Controllers/EntradasDetalleController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+    [ApiController]
+    public class EntradasDetalleController : ControllerBase
+    {   
+        
+    }
