0057a7d42335e676b78969b753a217a0031c84de
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-07-10 20:43:58 -0400

    Agregando signos de interrogacion a la propiedad EntradaId y ProductoId del modelo EntradasDetalle

--- Shared/EntradasDetalle.cs
+++ Shared/EntradasDetalle.cs
@@ -6,10 +6,10 @@ public class EntradasDetalle
     public int DetalleId { get; set; }
 
     [Required (ErrorMessage = "El Id de la entrada es obligatorio")]
-    public Entradas EntradaId { get; set; }
+    public Entradas? EntradaId { get; set; }
 
     [Required (ErrorMessage = "El Id del producto es obligatorio")]
-    public Productos ProductoId { get; set; }
+    public Productos? ProductoId { get; set; }
 
     [Required (ErrorMessage = "La cantidad utilizada es obligatoria")]
     public int CantidadUtilizada { get; set; }
